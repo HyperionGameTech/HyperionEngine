notomorrow/hyperion-engine

Add IRenderingAPI, VulkanRenderingAPI, etc. to allow for easier abstraction between rendering APIs

src/rendering/TemporalAA.hpp
@@ -32,7 +32,7 @@ public:
     void Resize(Vec2u resolution);
 
     void Create();
-    void Render(Frame *frame);
+    void Render(IFrame *frame);
 
 private:
     void CreateImages();

src/rendering/UIRenderer.hpp
@@ -45,8 +45,8 @@ public:
         const Optional<RenderableAttributeSet> &override_attributes = { }
     );
 
-    void CollectDrawCalls(Frame *frame);
-    void ExecuteDrawCalls(Frame *frame, const TResourceHandle<CameraRenderResource> &camera_resource_handle, const FramebufferRef &framebuffer) const;
+    void CollectDrawCalls(IFrame *frame);
+    void ExecuteDrawCalls(IFrame *frame, const TResourceHandle<CameraRenderResource> &camera_resource_handle, const FramebufferRef &framebuffer) const;
 
 private:
     Array<Pair<ID<Entity>, int>>    m_proxy_depths;
@@ -77,7 +77,7 @@ private:
     virtual void InitGame() override; // init on game thread
     virtual void OnRemoved() override;
     virtual void OnUpdate(GameCounter::TickUnit delta) override;
-    virtual void OnRender(Frame *frame) override;
+    virtual void OnRender(IFrame *frame) override;
 
     virtual void OnComponentIndexChanged(RenderSubsystem::Index new_index, RenderSubsystem::Index prev_index) override
         { AssertThrowMsg(false, "Not permitted!"); }

HyperionGameTech/HyperionEngine

src/rendering/TemporalAA.hpp
@@ -32,7 +32,7 @@ public:
     void Resize(Vec2u resolution);
 
     void Create();
-    void Render(Frame *frame);
+    void Render(FrameBase *frame);
 
 private:
     void CreateImages();

src/rendering/UIRenderer.hpp
@@ -45,8 +45,8 @@ public:
         const Optional<RenderableAttributeSet> &override_attributes = { }
     );
 
-    void CollectDrawCalls(Frame *frame);
-    void ExecuteDrawCalls(Frame *frame, const TResourceHandle<CameraRenderResource> &camera_resource_handle, const FramebufferRef &framebuffer) const;
+    void CollectDrawCalls(FrameBase *frame);
+    void ExecuteDrawCalls(FrameBase *frame, const TResourceHandle<CameraRenderResource> &camera_resource_handle, const FramebufferRef &framebuffer) const;
 
 private:
     Array<Pair<ID<Entity>, int>>    m_proxy_depths;
@@ -77,7 +77,7 @@ private:
     virtual void InitGame() override; // init on game thread
     virtual void OnRemoved() override;
     virtual void OnUpdate(GameCounter::TickUnit delta) override;
-    virtual void OnRender(Frame *frame) override;
+    virtual void OnRender(FrameBase *frame) override;
 
     virtual void OnComponentIndexChanged(RenderSubsystem::Index new_index, RenderSubsystem::Index prev_index) override
         { AssertThrowMsg(false, "Not permitted!"); }