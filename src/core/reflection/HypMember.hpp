67795e45b91718f0e6831574e6c3d2a160cadcb6
Andrew MacDonald <andrewmacdonald971@gmail.com>
2025-11-03 19:30:55 -0400

    Add PCH

--- src/core/reflection/HypMember.hpp
+++ src/core/reflection/HypMember.hpp
@@ -18,37 +18,63 @@ namespace hyperion {
 
 struct HypMember
 {
-    Variant<HypProperty, HypMethod, Field, HypConstant> value;
+    IHypMember* internal;
 
-    HypMember() = default;
+    HypMember()
+        : internal(nullptr)
+    {
+    }
 
     HypMember(HypProperty&& property)
-        : value(std::move(property))
+        : internal(new HypProperty(std::move(property)))
     {
     }
 
     HypMember(HypMethod&& method)
-        : value(std::move(method))
+        : internal(new HypMethod(std::move(method)))
     {
     }
 
     HypMember(Field&& field)
-        : value(std::move(field))
+        : internal(new Field(std::move(field)))
     {
     }
 
     HypMember(HypConstant&& field)
-        : value(std::move(field))
+        : internal(new HypConstant(std::move(field)))
     {
     }
 
     HypMember(const HypMember& other) = delete;
     HypMember& operator=(const HypMember& other) = delete;
 
-    HypMember(HypMember&& other) noexcept = default;
-    HypMember& operator=(HypMember&& other) noexcept = default;
+    HypMember(HypMember&& other) noexcept
+        : internal(other.internal)
+    {
+        other.internal = nullptr;
+    }
 
-    ~HypMember() = default;
+    HypMember& operator=(HypMember&& other) noexcept
+    {
+        if (this != &other)
+        {
+            delete internal;
+
+            internal = other.internal;
+            other.internal = nullptr;
+        }
+
+        return *this;
+    }
+
+    ~HypMember()
+    {
+        if (internal != nullptr)
+        {
+            delete internal;
+            internal = nullptr;
+        }
+    }
 };
 
 } // namespace hyperion
