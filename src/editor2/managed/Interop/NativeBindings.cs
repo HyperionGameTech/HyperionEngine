cccf7b5b742e6dbf93b2425d8d16b70100b0d411
Andrew MacDonald <mail@ajmd.dev>
2025-11-27 18:25:22 -0400

    Hyperion Editor v2 stuff

---
+++ src/editor2/managed/Interop/NativeBindings.cs
@@ -0,0 +1,9 @@
+using System;
+using System.Runtime.InteropServices;
+
+namespace Hyperion.Editor.Interop
+{
+    public static partial class NativeBindings
+    {
+    }
+}
--- src/editor2/managed/Program.cs
+++ src/editor2/managed/Program.cs
@@ -1,5 +1,7 @@
 using Avalonia;
 using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
 
 namespace Hyperion.Editor
 {
@@ -9,13 +11,16 @@ namespace Hyperion.Editor
         // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
         // yet and stuff might break.
         [STAThread]
-        public static void Main(string[] args) => BuildAvaloniaApp()
-            .StartWithClassicDesktopLifetime(args);
+        public static void Main(string[] args)
+        {
+            BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
+        }
 
         // Avalonia configuration, don't remove; also used by visual designer.
         public static AppBuilder BuildAvaloniaApp()
             => AppBuilder.Configure<App>()
                 .UsePlatformDetect()
+                .With(new MacOSPlatformOptions { ShowInDock = true })
                 .WithInterFont()
                 .LogToTrace();
     }
