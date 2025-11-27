8cb4be76e977b01858c4258cbba2269aca9a3a94
Andrew MacDonald <mail@ajmd.dev>
2025-11-27 16:43:19 -0400

    New editor first commit

---
+++ src/editor2/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Hyperion.Editor
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
