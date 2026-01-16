56fc4ec13d9f1aa284b90b2f47dd7a70d8a9ab48
Andrew MacDonald <mail@ajmd.dev>
2026-01-15 21:24:21 -0400

    Works

--- src/rendering/Shared.hpp
+++ src/rendering/Shared.hpp
@@ -1140,7 +1140,7 @@ struct DescriptorSetOffsetMap
 {
     HYP_STRUCT_BODY(DescriptorSetOffsetMap);
 
-    static constexpr uint32 MaxOffsets = 8;
+    static constexpr uint32 MaxOffsets = 16;
 
     StringHash keys[MaxOffsets];
     uint32 values[MaxOffsets];
