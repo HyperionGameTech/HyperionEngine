c36713da17e460cb32b87f90430c1758677df019
Andrew MacDonald <mail@ajmd.dev>
2026-04-11 21:12:17 -0300

    Removed foxtrot from external sources

--- External/ThirdParty/Source/Foxtrot/Asset/Fwd/Ax_Fwd_Manager.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <Asset/AxImage.hpp>
-
-namespace Fwd::AssetManager {
-
-FxTSRef<AxImage> LoadImageFromMemory(RxImageFormat format, const uint8* data, uint32 data_size);
-
-}; // namespace Fwd::AssetManager
