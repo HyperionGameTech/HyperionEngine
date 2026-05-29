8484d7500526796328a62c4c5813d1f8c83a417d
Andrew MacDonald <mail@ajmd.ca>
2026-05-29 16:57:06 -0300

    checkpoint 3 (missed files)

---
+++ Source/Engine/asset/SerializationUtils.hpp
@@ -0,0 +1,132 @@
+/*!
+ *  @author: The Hyperion Contributors
+ *  @date 2016-2026
+ *  @licence MIT
+*/
+
+#pragma once
+
+#include <Core/containers/String.hpp>
+#include <Core/containers/Map.hpp>
+
+#include <Core/utilities/Result.hpp>
+
+#include <Core/Defines.hpp>
+
+namespace Hyperion {
+
+struct BoxedValue;
+class Class;
+
+namespace utilities {
+struct TypeInfo;
+struct TypeId;
+} // namespace utilities
+
+using utilities::TypeId;
+using utilities::TypeInfo;
+
+namespace JSON {
+
+class Value;
+class Object;
+
+} // namespace JSON
+
+namespace functional {
+
+template <class FunctionSignature>
+class ProcRef;
+
+} // namespace functional
+
+using functional::ProcRef;
+
+struct ToJSONOptions
+{
+    enum class FollowAssetPathsMode : int
+    {
+        Never = -1,
+        MatchingAttribute = 0, // with "FollowAssetPath" attribute evaluating to true
+        Always
+    };
+
+    enum class SaveAssetsAsReferencesMode : int
+    {
+        No,
+        UnlessOtherwiseSpecified,
+        Yes
+    };
+
+    bool skipTransientProperties = true;
+    bool writeClassNames = false;
+    bool writeClassNamesRecursively = false;
+
+    SaveAssetsAsReferencesMode saveAssetsAsReferences = SaveAssetsAsReferencesMode::UnlessOtherwiseSpecified;
+    FollowAssetPathsMode followAssetPaths = FollowAssetPathsMode::MatchingAttribute;
+};
+
+/*! \brief Converts a BoxedValue to a Value.
+ *
+ *  \param value The BoxedValue to convert.
+ *  \param outJson The output Value.
+ *  \return True if conversion was successful, false otherwise.
+ */
+Result BoxedToJSON(
+    const BoxedValue& value,
+    JSON::Value& outJson,
+    ToJSONOptions opts = ToJSONOptions {});
+
+/*! \brief Serializes a Object instance to a Object.
+ *  Only fields and properties of the Class are serialized.
+ *  Members marked with the "jsonignore" attribute are skipped.
+ *  Members marked with the "transient" attribute are skipped if skipTransientProperties is true.
+ *  Members with the "jsonpath" attribute are serialized using the specified JSON path.
+ *  \param cls The Class of the object.
+ *  \param target The BoxedValue object to serialize.
+ *  \param outJson The output Object.
+ *  \return True if serialization was successful, false otherwise.
+ */
+Result ObjectToJSON(
+    const Class* cls,
+    const BoxedValue& target,
+    JSON::Object& outJson,
+    ToJSONOptions opts = ToJSONOptions {});
+
+/*! \brief Converts a JSON value to BoxedValue
+ *
+ *  \param jsonValue The Value to convert.
+ *  \param typeInfo The TypeInfo for the target type.
+ *  \param outBoxed The output BoxedValue.
+ *  \return True if conversion was successful, false otherwise.
+ */
+Result BoxedFromJSON(const JSON::Value& jsonValue, const TypeInfo& typeInfo, BoxedValue& outBoxed);
+
+/*! \brief Deserializes a Object to a BoxedValue object.
+ *  Only fields and properties of the Class are deserialized.
+ *  Members marked with the "jsonignore" attribute are skipped.
+ *  Members with the "jsonpath" attribute are deserialized using the specified JSON path.
+ *  \param jsonObject The Object to deserialize.
+ *  \param targetClass The desired Class of the target object. (can be null if target already has a type)
+ *  \param target The output BoxedValue object.
+ *  \return True if deserialization was successful, false otherwise.
+ */
+Result ObjectFromJSON(const JSON::Object& jsonObject, const Class* targetClass, BoxedValue& target);
+
+void WalkBoxedValue(
+    const BoxedValue& target,
+    const ProcRef<void(const BoxedValue& current)>& func);
+
+/*! \brief Resets all members marked with the Transient attribute to their default values.
+ *  Used when cloning objects to prevent transient runtime state from being copied.
+ *  \param value The BoxedValue to strip transient members from (modified in place). */
+void StripTransientMembers(BoxedValue& value);
+
+/*! \brief Creates a clone of the source BoxedValue with all transient members reset to defaults.
+ *  Creates a new default instance of the same type, then copies only non-transient members.
+ *  \param src The source BoxedValue to clone.
+ *  \param outDst The output BoxedValue (will be overwritten with the cleaned clone).
+ *  \return True if the clone was successful. */
+bool CloneWithoutTransientMembers(const BoxedValue& src, BoxedValue& outDst);
+
+} // namespace Hyperion
