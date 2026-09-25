da0dc5b2a358125dd662734457b29e7700b9757d
Andrew MacDonald <mail@ajmd.ca>
2026-09-25 13:00:35 -0300

    Remove testing anims and others from tracking.

--- Source/Engine/Framework/Commandlet/Commandlets/BuildShapesCommandlet.cpp
+++ Source/Engine/Framework/Commandlet/Commandlets/BuildShapesCommandlet.cpp
@@ -67,13 +67,13 @@ static void BuildThirdPersonCharacter(Handle<AssetRegistry>& engineRegistry)
 {
     GlobalContextScope assetRegistryScope { AssetRegistryContext { engineRegistry } };
 
-    auto characterPrefabResult = g_assetManager->Load<Prefab>("Models/Mannequin/Mannequin.glb",
+    auto characterPrefabResult = g_assetManager->Load<Prefab>("Models/Hazmat/Hazmat.glb",
         String::empty,
         AssetLoadHint::Transient);
 
     if (!characterPrefabResult.HasValue())
     {
-        HYP_LOG(Engine, Error, "Failed to load source Models/Mannequin/Mannequin.glb to build the ThirdPersonCharacter prefab");
+        HYP_LOG(Engine, Error, "Failed to load source Models/Hazmat/Hazmat.glb to build the ThirdPersonCharacter prefab");
 
         return;
     }
